d933598eda2a75a8b5327556061d4f1f0e6c3da7
Mhankk <57066081+Mhankk@users.noreply.github.com>
2021-08-19 23:30:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -40,12 +40,12 @@ client.initialize();
 client.on('message', message => {
 
     //Random Chat
-    var aa = ["cek","Cek","tc","Tc"];
-    //if(message.body(text)) {
+    var aa = ["cek","Cek","tc","Tc","p","P"];
+    //if(message.body('text')) {
         if (aa.includes(message.body)) {
         message.reply('Online!');
     }
-    var bb = ["hai","Hai","halo","Halo"];
+    var bb = ["hai","Hai","halo","Halo","hallo","Hallo"];
         if (bb.includes(message.body)) {
         message.reply('Whatsup?');
     }
@@ -53,13 +53,33 @@ client.on('message', message => {
         if (cc.includes(message.body)) {
         message.reply('link apa nih? bagi dong hehe');
     }
+    var dd = ["ajg","anjg","cok","Cok","asw","asu","Asw","Asu","ajg emg","Ajg emg","bgsd","bgsd emg","Bgsd"];
+        if (dd.includes(message.body)) {
+        message.reply('busett ngegadd anjer :v');
+    }
+    var ee = ["Apa?","apa?","apa","Apa","paan","apaan?","Apaan?","apaan","ada apa?","Ada apa?"];
+        if (ee.includes(message.body)) {
+        message.reply('lah apa??');
+    }
+    var ff = ["Yoi","yoi","iya","Iya","iya dong","Iya dong"];
+        if (ff.includes(message.body)) {
+        message.reply('hmmmm');
+    }
+    var gg = ["hehehe","hehe","xixixi","hihihi","hahaha","wkwkwk"];
+        if (gg.includes(message.body)) {
+        message.reply('tawa lu bgsd');
+    }
+    var hh = ["dc?","Dc?","dc gan","Dc gan","Dc gan?","Discord?"];
+        if (hh.includes(message.body)) {
+        message.reply('Kuyylah');
+    }
 
     //With Command
     if (message.body === '!link') {
         message.reply('*Wibuclips Link*\n\nInstagram : https://instagram.com/wibuclips\nDiscord : https://discord.gg/5MJZfBYcvA\nPartner : https://instagram.com/dokteranime');
     }
     if (message.body === '!rules') {
-        message.reply('*Wibuclips Rules*\n\n◦ Dont share porn\n◦ Dont share promotion link\n◦ Dont share scam link\n◦ You must 14 y.o+\n◦ Dont spam message');
+        message.reply('*Wibuclips Rules*\n\nâ—¦ Dont share porn\nâ—¦ Dont share promotion link\nâ—¦ Dont share scam link\nâ—¦ You must 14 y.o+\nâ—¦ Dont spam message');
     }
     if (message.body === '!admin') {
         message.reply(`@${6288228794134}\n@${6285263370991}\n@${6281299385684}\n@${62895355480683}\n@${6285156316414}`, {
@@ -67,7 +87,7 @@ client.on('message', message => {
         });
     }
     if (message.body === '!help') {
-        message.reply('*Bot Commands*\n\n*!rules*\n◦ menampilkan peraturan wibuclips\n*!link*\n◦ menampilkan link wibuclips\n\nJangan lupa follow ig @wibuclips')
+        message.reply('*Bot Commands*\n\n*!rules*\nâ—¦ menampilkan peraturan wibuclips\n*!link*\nâ—¦ menampilkan link wibuclips\n\nJangan lupa follow ig @wibuclips')
     }
 });
 
